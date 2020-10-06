197d497dbb29b3390824a0482e7b2e659a79af99
Sergey Maslov <sergej.serzh-m@yandex.ru>
2020-10-06 18:48:46 +0300

    commit 4

--- src/main/java/org/maslov/info/interfaces/controller/HomeController.java
+++ src/main/java/org/maslov/info/interfaces/controller/HomeController.java
@@ -2,18 +2,21 @@ package org.maslov.info.interfaces.controller;
 
 import org.maslov.info.interfaces.*;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
-@RestController
+@Controller
 public class HomeController {
     private org.springframework.context.annotation.AnnotationConfigApplicationContext context;
 
     @RequestMapping(value = "/")
-    public String home() {
+    public String home(Model model) {
         context = new AnnotationConfigApplicationContext(JavaConfig.class);
         Sender sender = context.getBean("wirelessSenderImpl", Sender.class);
-        return sender.sendMessage("Message from Java config");
+        model.addAttribute("msg", sender.sendMessage("Message from Java config"));
+        return "test";
     }
 }
